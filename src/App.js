a3e4d5fcd85c0a17b645a31c57ea4623c5738ae5
Attila Jakab <Attila.Jakab@infor.com>
2018-03-07 20:22:55 +0100

    apollo request added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
+
+
+const GET_FLIGHTS = gql`
+  query {
+    allFlights(search:{from: {location: "PRG"}, to: {location: "BCN"}, date:{exact:"2018-04-01"}}){
+      edges {
+        node {
+          id,
+          airlines {
+            name
+            code
+            logoUrl
+            isLowCost
+          },
+          departure {
+            time
+            localTime
+            airport{
+              name,
+              city{
+                name
+              }
+            }
+          },
+          arrival {
+            time
+            localTime,
+            airport{
+              name,
+              city{
+                name
+              }
+            }
+          },
+          duration,
+          legs {
+            id,
+            arrival {
+              time
+              localTime
+            },
+            departure {
+              time
+              localTime
+            },
+            duration,
+            flightNumber,
+            recheckRequired
+          },
+          price {
+            amount
+            currency
+          }
+        }
+      }
+    }
+  }
+  `;
+
+
 
 class App extends Component {
 
@@ -17,9 +79,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Query query={GET_FLIGHTS}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Loading...</div>;
+          if (error) return <div>Error :(</div>;
+          
+          return (
+            <div>{data[0]}</div>
+          )
+        }}
+      </Query>
     );
   }
 }
